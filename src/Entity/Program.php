cd427ccac7113184a4c779b07fcb872b0cb193de
johnnyhelloworld <johnny58723826@hotmail.fr>
2022-01-07 20:03:48 +0100

    challenge12: Category and Program validations

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,8 +6,11 @@ use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: ProgramRepository::class)]
+#[UniqueEntity('title', message: "This title already exists")]
 class Program
 {
     #[ORM\Id]
@@ -16,9 +19,13 @@ class Program
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: "Input cannot be empty")]
+    #[Assert\Length(max: 255, maxMessage:"The title program {{ value }} is too long, it cannot contains more than {{ limit }} caracters")]
     private $title;
 
     #[ORM\Column(type: 'text')]
+    #[Assert\NotBlank(message: "Input cannot be empty")]
+    #[Assert\Regex(pattern: "/plus belle la vie/", match: false, message: "We're talking about real programs")]
     private $summary;
 
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
